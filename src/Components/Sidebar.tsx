2e84afbdcd25e024a7127e7fbe5e899cab1be912
Aadish81 <aadish81@AADISH.arj>
2025-06-26 19:11:27 +0545

    conditional CSS removed form nav

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -13,22 +13,13 @@ const  Sidebar:React.FC<Bar> = ({setOpenMenu, openMenu}) => {
 
 
   return (
-    <nav className={`fixed  top-0 z-40 w-[100%] h-screen backdrop-blur-sm  
-                    transition-all duration-1000 ease-in-out
-                    ${
-                      openMenu?
-                      "h-screen opacity-100 pointer-events-auto":
-                      "h-0 opacity-0 pointer-events-none"
-                    } 
-
-                  
-    `}>
+    <nav className={`fixed  top-0 z-40 w-[100%] h-screen   backdrop-blur-sm slider`}>
         <div className='flex flex-col justify-center items-center gap-8  font-mono '>
-            <div className='h-16  flex justify-start items-center '><span className=' font-bold hover:cursor-pointer text-lg' onClick={()=>setOpenMenu(false)} >&#10006;</span></div>
-            <a href="#Home" className={`text-gray-400  hover:text-white  text-lg font-bold transform transition-transform duration-500 ${openMenu?"opacity-100 translate-y-0":"opacity-0 translate-y-5"}`} onClick={()=>setOpenMenu(false)}>Home</a>
-            <a href="#About"className={`text-gray-400  hover:text-white  text-lg font-bold transform transition-transform duration-500 ${openMenu?"opacity-100 translate-y-0":"opacity-0 translate-y-5"}`} onClick={()=>setOpenMenu(false)}>About</a>
-            <a href="#Skills"className={`text-gray-400  hover:text-white  text-lg font-bold transform transition-transform duration-500 ${openMenu?"opacity-100 translate-y-0":"opacity-0 translate-y-5"}`} onClick={()=>setOpenMenu(false)}>Skills</a>
-            <a href="#Contact"className={`text-gray-400  hover:text-white  text-lg font-bold transform transition-transform duration-500 ${openMenu?"opacity-100 translate-y-0":"opacity-0 translate-y-5"}`} onClick={()=>setOpenMenu(false)}>Contact</a>
+            <div className='h-16  flex justify-start items-center '><span className=' text-gray-400 hover:text-white font-bold hover:cursor-pointer text-2xl' onClick={()=>setOpenMenu(false)} >&#10006;</span></div>
+            <a href="#Home" className={`text-gray-400  hover:text-white  text-lg font-bold hover:-translate-y-2 transition-transform duration-500 ${openMenu?"opacity-100 translate-y-0 ":"opacity-0 translate-y-5"}`} onClick={()=>setOpenMenu(false)}>Home</a>
+            <a href="#About"className={`text-gray-400  hover:text-white  text-lg font-bold hover:-translate-y-2 transition-transform duration-500 ${openMenu?"opacity-100 translate-y-0":"opacity-0 translate-y-5"}`} onClick={()=>setOpenMenu(false)}>About</a>
+            <a href="#Skills"className={`text-gray-400  hover:text-white  text-lg font-bold hover:-translate-y-2 transform transition-transform duration-500 ${openMenu?"opacity-100 translate-y-0":"opacity-0 translate-y-5"}`} onClick={()=>setOpenMenu(false)}>Skills</a>
+            <a href="#Contact"className={`text-gray-400  hover:text-white  text-lg font-bold hover:-translate-y-2 transform transition-transform duration-500 ${openMenu?"opacity-100 translate-y-0":"opacity-0 translate-y-5"}`} onClick={()=>setOpenMenu(false)}>Contact</a>
         </div>
 
     </nav>
